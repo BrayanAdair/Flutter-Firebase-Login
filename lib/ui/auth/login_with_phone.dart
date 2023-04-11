1298d1a462cba4fa31e48f6a5da9ec77dc92bbbc
BrayanAdair <brayanadairmontano083@gmail.com>
2023-04-11 09:15:07 -0600

    Login y Verificacion por SMS

--- lib/ui/auth/login_with_phone.dart
+++ lib/ui/auth/login_with_phone.dart
@@ -1,9 +1,9 @@
+
+import 'package:login/ui/auth/verify_code.dart';
+import 'package:login/utils/utils.dart';
+import 'package:login/widget/round_button.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_login/flutter_login.dart';
-import 'package:login/ui/auth/verify_code.dart';
-import 'package:login/widget/round_button.dart';
-import '../../utils/utils.dart';
 
 class LoginWithPhoneNumber extends StatefulWidget {
   const LoginWithPhoneNumber({super.key});
@@ -16,22 +16,25 @@ class _LoginWithPhoneNumberState extends State<LoginWithPhoneNumber> {
   bool loading = false;
   final phoneNumberController = TextEditingController();
   final auth = FirebaseAuth.instance;
-@override
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Login'),
+        title: Text("Login with Phone"),
       ),
       body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 20),
+        padding: const EdgeInsets.all(8.0),
         child: Column(children: [
           SizedBox(
             height: 50,
           ),
           TextFormField(
             controller: phoneNumberController,
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(hintText: '+52 771 1896 273'),
+            keyboardType: TextInputType.phone,
+            decoration: InputDecoration(
+              hintText: '+52 556 50 48 969',
+            ),
           ),
           SizedBox(
             height: 80,
@@ -45,35 +48,35 @@ class _LoginWithPhoneNumberState extends State<LoginWithPhoneNumber> {
                 auth.verifyPhoneNumber(
                     phoneNumber: phoneNumberController.text,
                     verificationCompleted: (_) {
-                                      setState(() {
-                  loading = false;
-                });
-                
+                      setState(() {
+                        loading = false;
+                      });
                     },
                     verificationFailed: (e) {
-                                      setState(() {
-                  loading = false;
-                });
+                      setState(() {
+                        loading = false;
+                      });
                       Utils().toastMessage(e.toString());
                     },
-                    codeSent: (String VerificationId, int? Token) {
+                    codeSent: (String verificationId, int? token) {
                       Navigator.push(
                           context,
                           MaterialPageRoute(
                               builder: (context) => VerifyCodeScreen(
-                                    verificationId: VerificationId,
+                                    verificationId: verificationId,
                                   )));
-                                                  setState(() {
-                  loading = true;
-                });
+                                   setState(() {
+                        loading = false;
+                      });
                     },
                     codeAutoRetrievalTimeout: (e) {
-                                      setState(() {
-                  loading = true;
-                });
+                       
                       Utils().toastMessage(e.toString());
+                      setState(() {
+                        loading = false;
+                      });
                     });
-              })
+              }),
         ]),
      ),
 );
--- lib/ui/auth/verify_code.dart
+++ lib/ui/auth/verify_code.dart
@@ -1,11 +1,9 @@
+import 'package:login/ui/auth/posts/post_screen.dart';
+
+import 'package:login/utils/utils.dart';
+import 'package:login/widget/round_button.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_login/flutter_login.dart';
-import 'package:login/ui/auth/posts/post_screen.dart';
-import 'package:login/widget/round_button.dart';
-
-import '../../utils/utils.dart';
-
 
 class VerifyCodeScreen extends StatefulWidget {
   final String verificationId;
@@ -16,63 +14,60 @@ class VerifyCodeScreen extends StatefulWidget {
 }
 
 class _VerifyCodeScreenState extends State<VerifyCodeScreen> {
+  
   bool loading = false;
-  // final phoneNumberController = TextEditingController();
-  final verificationCodeController = TextEditingController();
+  final verifyCodeController = TextEditingController();
   final auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+      return Scaffold(
       appBar: AppBar(
-        title: Text("Verify"),
+        
+        title: Text('Verify'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          
+          children: [
+            SizedBox(
+              height: 100,
+            ),
+            TextFormField(
+              controller: verifyCodeController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(hintText: '6 Digitos'),
+            ),
+            SizedBox(
+              height: 100,
+            ),
+            RoundButton(title: 'Verify', 
+            loading: loading,
+            onTap: ()async {
+              setState(() {
+                loading = true;
+              });
+              final credential = PhoneAuthProvider.credential(
+                verificationId: widget.verificationId,
+                smsCode: 
+                verifyCodeController.text.toString()
+              );
+              try {
+                await auth.signInWithCredential(credential);
+                Navigator.push(context, 
+                  MaterialPageRoute(builder: (context)=>
+                  PostScreen()
+                  ));
+              } catch (e) {
+                setState(() {
+                  loading = false;
+                });
+                Utils().toastMessage(e.toString());
+              }
+            })     
+          ],
         ),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 20),
-          child: Column(
-            children: [
-              SizedBox(
-                height: 100,
-              ),
-              TextFormField(
-                // controller: phoneNumberController,
-                controller: verificationCodeController,
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(hintText: '6 digitos'),
-              ),
-              SizedBox(
-                height: 100,
-              ),
-              RoundButton(
-                title: 'Verify',
-                loading: loading,
-                onTap: () async {
-                  setState(() {
-                    loading = true;
-                  });
-                  final credential = PhoneAuthProvider.credential(
-                    verificationId: widget.verificationId,
-                    smsCode: verificationCodeController.text.toString(),
-                    // aqui no se si va fuera del final
-                    // end del no se si va fuera
-           
-                  );
-                  try {
-                    await auth.signInWithCredential(credential);
-                    Navigator.push(context,
-                    MaterialPageRoute(builder: (context) =>
-                    PostScreen()));
-                  } catch (e) {
-                    setState(() {
-                      loading = false;
-                    });
-                    Utils().toastMessage(e.toString());
-                  }
-                  // pña
-                },
-              )
-            ],
-          ),
-        ),
-    );
-  }
+     ),
+);
+}
 }
